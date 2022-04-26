9b77147d1c2259d229de176fb33a70bb38d2ce1a
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-04-26 14:21:26 +0530

    react basic setup done

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+   <>
+   <h1>Hello I am Ao</h1>
+   </>
+  )
+}
+
+export default App
---
+++ client/src/components/counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <>
+    <div className="counter_container">
+        
+    </div>
+    </>
+  )
+}
+
+export default Counter
